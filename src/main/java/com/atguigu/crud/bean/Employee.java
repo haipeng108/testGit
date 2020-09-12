421ad8d8aea07bd8e4cdd137117c8fd36a58b3f5
haipeng108 <71173039+haipeng108@users.noreply.github.com>
2020-09-12 18:24:44 +0800

    在线添加

--- src/main/java/com/atguigu/crud/bean/Employee.java
+++ src/main/java/com/atguigu/crud/bean/Employee.java
@@ -51,14 +51,16 @@ public class Employee {
         this.dId = dId;
     }
     public static void main(String[] args) {
-    	System.out.println("更新1");
-    	System.out.println("更新2");
-    	System.out.println("创建分支");
-    	System.out.println("创建分支2");
+    	System.out.println("鏇存柊1");
+    	System.out.println("鏇存柊2");
+    	System.out.println("鍒涘缓鍒嗘敮");
+    	System.out.println("鍒涘缓鍒嗘敮2");
 
-    	System.out.println("主线添加");
+    	System.out.println("涓荤嚎娣诲姞");
 
-    	System.out.println("分支添加");
+    	System.out.println("鍒嗘敮娣诲姞");
+        System.out.println("鍦ㄧ嚎娣诲姞");
+
 
     } 
 }
